72d92788e3803b7deb0bc2d4a55ed2dba41dde40
Marko Šeparović <maseparov@vsite.hr>
2024-01-18 18:18:12 +0100

    Add project files.

---
+++ PINLab5/Data/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace PINLab5.Data
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
